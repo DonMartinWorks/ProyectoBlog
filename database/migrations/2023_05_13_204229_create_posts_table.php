62476d284b793f42f62d33ca5aee9594fe80860d
DonMartinWorks <donmartinworks@gmail.com>
2023-05-15 17:34:02 -0400

    [Nueva Configuracion] - Datos unique para las migraciones

--- database/migrations/2023_05_13_204229_create_posts_table.php
+++ database/migrations/2023_05_13_204229_create_posts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->string('name', 100)->unique();
             $table->string('slug', 100);
             $table->text('extract');
             $table->longText('body');
