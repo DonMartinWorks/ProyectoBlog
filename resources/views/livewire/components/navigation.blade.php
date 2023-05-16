c701fc1a8d0e2bc7cb82d79eaf6bb2f91e743c52
DonMartinWorks <donmartinworks@gmail.com>
2023-05-16 12:08:00 -0400

    [Logout] - Se configura el logout para NAVIGATION

--- resources/views/livewire/components/navigation.blade.php
+++ resources/views/livewire/components/navigation.blade.php
@@ -91,15 +91,23 @@
                             To: "transform opacity-0 scale-95"
 
                     -->
-                    <div x-show="open" x-on:click.away="open = false" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    <div x-show="open" x-on:click.away="open = false"
+                        class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                         role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                         <!-- Active: "bg-gray-100", Not Active: "" -->
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
                             id="user-menu-item-0">Your Profile</a>
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
                             id="user-menu-item-1">Settings</a>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                            id="user-menu-item-2">Sign out</a>
+
+                        <!-- Logout - Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">@csrf
+                            <a href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                        this.closest('form').submit();"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-rose-600 hover:text-white"
+                                role="menuitem" tabindex="-1" id="user-menu-item-2">{{ __('Log Out') }}</a>
+                        </form>
                     </div>
                 </div>
             </div>
