76759ed78dc6ed47660b2ce1b1ff14f8bf866b52
DonMartinWorks <donmartinworks@gmail.com>
2023-05-16 20:01:20 -0400

    [Cambios Factory] - Cambios para los datos

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -19,7 +19,7 @@ class TagFactory extends Factory
      */
     public function definition(): array
     {
-        $name = $this->faker->unique()->company();
+        $name = $this->faker->unique()->word();
 
         return [
             'name' => $name,
