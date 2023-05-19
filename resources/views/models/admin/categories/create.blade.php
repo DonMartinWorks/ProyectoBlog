bca7621c5df9c0d6c03c372d3ba44ca335d25e65
DonMartinWorks <donmartinworks@gmail.com>
2023-05-19 12:01:13 -0400

    [Funcionalidad Slug] - Se agrega la funcionalidad de slug para category

--- resources/views/models/admin/categories/create.blade.php
+++ resources/views/models/admin/categories/create.blade.php
@@ -17,7 +17,7 @@
 
             <div class="form-group">
                 {!! Form::label('slug', __('Slug')) !!}
-                {!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'Enter category slug']) !!}
+                {!! Form::text('slug', null, ['class' => 'form-control', 'readonly']) !!}
             </div>
 
             <div class="text-center mt-5">
@@ -28,3 +28,17 @@
         </div>
     </div>
 @stop
+
+@section('js')
+    <script src="{{ asset('vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            $("#name").stringToSlug({
+                setEvents: 'keyup keydown blur',
+                getPut: '#slug',
+                space: '-'
+            });
+        });
+    </script>
+@endsection
