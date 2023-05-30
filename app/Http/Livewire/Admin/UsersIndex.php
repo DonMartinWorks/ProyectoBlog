0c8e3a3bee6af7c3f03757189474677b71edd073
DonMartinWorks <donmartinworks@gmail.com>
2023-05-30 12:30:38 -0400

    [Tabla Users] - Funcionalidad del filtro de búsqueda

--- app/Http/Livewire/Admin/UsersIndex.php
+++ app/Http/Livewire/Admin/UsersIndex.php
@@ -13,9 +13,24 @@ class UsersIndex extends Component
 
     protected $paginationTheme = 'bootstrap';
 
+    /**
+     * Esta variable se encarga de buscar en la base de datos los posts que coincidan
+     * con lo que se ingresa, lo que quiere decir que si el usuario ingresa un caracter
+     * o una cadena de estos, segun esta busqueda trae los posts (segun el name) que
+     * coincidan en la base de datos.
+     */
+    public $search;
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
-        $users = User::paginate();
+        $users = User::where('name', 'LIKE', '%' . $this->search . '%')
+            ->orWhere('email', 'LIKE', '%' . $this->search . '%')
+            ->paginate();
         return view('livewire.admin.users-index', compact('users'));
     }
 }
--- resources/views/livewire/admin/users-index.blade.php
+++ resources/views/livewire/admin/users-index.blade.php
@@ -1,46 +1,59 @@
 <div>
     <div class="card">
-        <div class="card-body">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>{{ __('ID') }}</th>
-                        <th>{{ __('Name') }}</th>
-                        <th>{{ __('Email') }}</th>
-                        <th></th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    @foreach ($users as $user)
-                        <tr>
-                            <td>{{ $user->id }}</td>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->email }}</td>
-
-                            <td width="10px">
-                                <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-warning btn-sm"
-                                    title="{{ __('Edit') }} {{ $user->name }}">{{ __('Edit') }}</a>
-                            </td>
-                            <td width="10px">
-                                <form action="{{ route('admin.users.destroy', $user) }}" method="post"
-                                    onsubmit="return confirm('Are you sure you want to delete this user?');">
-                                    @csrf
-                                    @method('delete')
-                                    <button title="{{ __('Delete') }} {{ $user->name }}" type="submit"
-                                        class="btn btn-danger btn-sm">{{ __('Delete') }}</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <div class="card-header">
+            <input wire:model="search" class="form-control"
+                placeholder="{{ __('If you want find a user, write the name or email here') }}">
         </div>
 
-        <div class="card-footer">
-            <div class="pagination justify-content-center">
-                {{ $users->links() }}
+        @if ($users->count())
+            <div class="card-body">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>{{ __('ID') }}</th>
+                            <th>{{ __('Name') }}</th>
+                            <th>{{ __('Email') }}</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+
+                                <td width="10px">
+                                    <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-warning btn-sm"
+                                        title="{{ __('Edit') }} {{ $user->name }}">{{ __('Edit') }}</a>
+                                </td>
+                                <td width="10px">
+                                    <form action="{{ route('admin.users.destroy', $user) }}" method="post"
+                                        onsubmit="return confirm('Are you sure you want to delete this user?');">
+                                        @csrf
+                                        @method('delete')
+                                        <button title="{{ __('Delete') }} {{ $user->name }}" type="submit"
+                                            class="btn btn-danger btn-sm">{{ __('Delete') }}</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
+
+            <div class="card-footer">
+                <div class="pagination justify-content-center">
+                    {{ $users->links() }}
+                </div>
+            </div>
+    </div>
+    @else
+    <div class="card-body">
+        <div class="text-center mt-5">
+            <strong>{{ __('We do not find any user') }}</strong>
         </div>
     </div>
+    @endif
 </div>
